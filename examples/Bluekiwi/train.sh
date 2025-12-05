1585405d6c91b3ba2230108c31f25535a28823bb
PinkOcelot-0w0 <644360357@qq.com>
2025-12-05 23:22:58 +0800

    1

--- examples/Bluekiwi/train.sh
+++ examples/Bluekiwi/train.sh
@@ -1,5 +1,5 @@
 #训练 SmolVLA 策略
-lerobot-train --dataset.repo_id=PinkOcelot/lekiwi_vladata --policy.type=smolvla --policy.empty_cameras=1 --batch_size=12 --steps=30000 --output_dir=outputs/smolvla --job_name=my_smolvla_training --policy.device=cuda --wandb.enable=false --policy.repo_id=pinkocelot/smolvla-latest --save_freq=5000
+lerobot-train --dataset.repo_id=PinkOcelot/lekiwi_box_merged --policy.type=smolvla --policy.empty_cameras=1 --batch_size=8 --steps=30000 --output_dir=outputs/smolvla --job_name=my_smolvla_training --policy.device=cuda --wandb.enable=false --policy.repo_id=pinkocelot/smolvla-latest --save_freq=5000
 #训练 ACT 策略
 lerobot-train --dataset.repo_id=PinkOcelot/lekiwi_vladata --policy.type=act --output_dir=outputs/act_model --job_name=act --policy.device=cuda --wandb.enable=false --policy.repo_id=${HF_USER}/act_policy --batch_size=8 --steps=100000 --save_freq=10000
 #启动策略服务
