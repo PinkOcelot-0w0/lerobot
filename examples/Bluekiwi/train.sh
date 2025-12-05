c6088746a1438a1fdc19be02ebf3b0ef3a9d0493
PinkOcelot-0w0 <644360357@qq.com>
2025-12-05 21:09:36 +0800

    修改训练脚本

--- examples/Bluekiwi/train.sh
+++ examples/Bluekiwi/train.sh
@@ -1,5 +1,8 @@
-lerobot-train --dataset.repo_id=PinkOcelot/lekiwi_vladata --policy.type=smolvla --policy.load_vlm_weights=true --policy.train_expert_only=true --policy.empty_cameras=1 --policy.max_action_dim=32 --batch_size=8 --steps=20000 --output_dir=outputs/smolvla --job_name=my_smolvla_training --policy.device=cuda --wandb.enable=false --policy.repo_id=pinkocelot/smolvla-latest --save_freq=4000
-
+#训练 SmolVLA 策略
+lerobot-train --dataset.repo_id=PinkOcelot/lekiwi_vladata --policy.type=smolvla --policy.empty_cameras=1 --batch_size=16 --steps=30000 --output_dir=outputs/smolvla --job_name=my_smolvla_training --policy.device=cuda --wandb.enable=false --policy.repo_id=pinkocelot/smolvla-latest --save_freq=5000
+#训练 ACT 策略
 lerobot-train --dataset.repo_id=PinkOcelot/lekiwi_vladata --policy.type=act --output_dir=outputs/act_model --job_name=act --policy.device=cuda --wandb.enable=false --policy.repo_id=${HF_USER}/act_policy --batch_size=8 --steps=100000 --save_freq=10000
-
+#启动策略服务
 python -m lerobot.async_inference.policy_server --host=192.168.1.104 --port=8080
+#合并数据集
+lerobot-edit-dataset --repo_id PinkOcelot/lekiwi_box_merged --operation.type merge --operation.repo_ids "['PinkOcelot/lekiwi_box', 'PinkOcelot/lekiwi_box']"
---
+++ hf_offline_on.bat
@@ -0,0 +1,6 @@
+@echo off
+setx HF_HUB_OFFLINE 1
+setx HF_DATASETS_OFFLINE 1
+setx TRANSFORMERS_OFFLINE 1
+echo HuggingFace 已切换到离线模式。
+pause
