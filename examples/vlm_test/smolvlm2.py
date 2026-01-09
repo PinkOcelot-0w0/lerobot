3c3b594a4d432513c12736d9f962c3b6c2792b8b
PinkOcelot-0w0 <644360357@qq.com>
2026-01-09 21:09:46 +0800

    合并上游代码

--- examples/vlm_test/smolvlm2.py
+++ examples/vlm_test/smolvlm2.py
@@ -1,35 +1,37 @@
 from transformers import AutoProcessor, AutoModelForImageTextToText
 import torch
- 
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+print(DEVICE)
 model_path = "models/extracted_vlm"
 processor = AutoProcessor.from_pretrained(model_path)
 model = AutoModelForImageTextToText.from_pretrained(
     model_path,
-    torch_dtype=torch.bfloat16,
-    _attn_implementation="flash_attention_2"
-).to("cuda")
- 
+    dtype=torch.bfloat16,
+    _attn_implementation="eager",
+   device_map="cuda").to(DEVICE)
+
 messages = [
     {
         "role": "user",
         "content": [
-            {"type": "text", "text": "描述一下这张图片"},
-            {"type": "image", "path": "vlm_test/images/1.jpg"}
+            {"type": "image", "url": "/home/pinkocelot/code/lerobot/examples/vlm_test/images/inbox.png"},
+            {"type": "text", "text": "纸巾在盒子里吗"},
         ]
     },
 ]
- 
+
 inputs = processor.apply_chat_template(
     messages,
     add_generation_prompt=True,
     tokenize=True,
     return_dict=True,
     return_tensors="pt",
-).to(model.device)
- 
+).to(model.device, dtype=torch.bfloat16)
+
 generated_ids = model.generate(**inputs, do_sample=False, max_new_tokens=64)
 generated_texts = processor.batch_decode(
     generated_ids,
     skip_special_tokens=True,
 )
- 
+print(generated_texts[0])
